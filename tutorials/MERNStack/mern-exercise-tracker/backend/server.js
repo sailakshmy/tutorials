6bc0e0534f8a70b143206cd40ce74bcf01478c6c
sailakshmy <sailakshmy94@gmail.com>
2020-08-09 02:31:22 +0530

    1)Connected to DB and Created a DB Schema
    2) Created files to add API endpoint routes

--- tutorials/MERNStack/mern-exercise-tracker/backend/server.js
+++ tutorials/MERNStack/mern-exercise-tracker/backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors =  require('cors');
+const mongoose = require('mongoose'); // So that we can use mongoose to connect to mongoDB Atlas 
 
 require('dotenv').config();//So that we can have our environment variables in the .env file
 
@@ -8,6 +9,21 @@ const port = process.env.PORT || 5000; //Port that the server will be on
 
 app.use(cors()); // cors middleware
 app.use(express.json()); // allows us to parse JSON as the server is going to be sending and receiving JSON
+
+const uri = process.env.ATLAS_URI;// Db's URI. Something that we have to get from the MongoDBAtlas Dashboard
+mongoose.connect(uri,//This is where our Database is stored. Mongoose.connect is how we start our connection
+    {useNewUrlParser: true,  useCreateIndex: true});// These 2 flags are basically added to handle MongoDB updates
+const connection =  mongoose.connection;
+connection.once('open',()=>{
+    console.log("MongoDB database connection established successfully");
+});//This is basically executed once the connection is open
+
+//Informing the server to use the following route files
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);// IF a user goes to the /exercise route, we will load everything that is there in exercisesRouter
+app.use('/users', usersRouter);
 
 app.listen(port,()=>{
     console.log(`Server is running on port: ${port}`);
